a1759dcb0b7bf2eee00e7d0b16fedecf7cc11fb0
Bakhaow <safwanechahboune@gmail.com>
2021-09-12 00:47:44 +0200

    Update render.c
    + Patch drawTri

--- src/render/render.c
+++ src/render/render.c
@@ -41,30 +41,52 @@ void drawHollowedTri(SDL_Renderer* renderer, int x[], int y[], int z[], int colo
 	SDL_RenderDrawLine(renderer, x[0], x[1], y[0], y[1]);
 	SDL_RenderDrawLine(renderer, y[0], y[1], z[0], z[1]);
 	SDL_RenderDrawLine(renderer, z[0], z[1], x[0], x[1]);
+	SDL_SetRenderDrawColor(renderer, 50, 255, 50, alpha);
+	for (int i = x[0]; i < x[1]; i++) {
+		SDL_RenderDrawLine(renderer, x[0] + i, x[1] + i, z[0], z[1]);
+	}
 }
 
 void drawTri(SDL_Renderer* renderer, int x[], int y[], int z[], int color[], int alpha)
 {
-	drawHollowedTri(renderer, x, y, z, color, alpha);
-	SDL_SetRenderDrawColor(renderer, 50, color[1], 50, alpha);
-	for (int i = x[0]; i < x[1]; i++) {
-		SDL_RenderDrawLine(renderer, x[0] + i, x[1], z[0], z[1]);
+	SDL_SetRenderDrawColor(renderer, 50, 50, 50, alpha);
+	if (x[1] == z[1]) {
+		int a = x[0];
+		int b = z[0];
+		if (x[0] > z[0]) {
+			a = z[0];
+			b = x[0];
+		}
+		for (int i = a; i < b; i++) {
+			SDL_RenderDrawLine(renderer, i, x[1], y[0], y[1]);
+		}
+	}
+	else {
+		int a = x[1];
+		int b = z[1];
+		if (x[1] > z[1]) {
+			a = z[1];
+			b = x[1];
+		}
+		for (int i = a; i < b; i++) {
+			SDL_RenderDrawLine(renderer, x[0], i, y[0], y[1]);
+		}
 	}
 }
 
-void draw(SDL_Renderer* Renderer)
+void draw(SDL_Renderer* renderer)
 {
-	SDL_RenderClear(Renderer);
+	SDL_RenderClear(renderer);
 
-	int x[] = { 100, 100 };
-	int y[] = { 300, 100 };
-	int z[] = { 100, 300 };
-	int color[] = { 255, 255, 255 };
+	unsigned int x[] = { 100, 100 };
+	unsigned int y[] = { 300, 100 };
+	unsigned int z[] = { 100, 300 };
+	unsigned int color[] = { 255, 255, 255 };
 
 	//draw a square from 2 tri-angles
-	drawTri(Renderer, x, y, z, color, 0xFF);
+	drawTri(renderer, x, y, z, color, 0xFF);
 	x[0] = 300; x[1] = 300;
-	drawTri(Renderer, x, y, z, color, 0xFF);
+	drawTri(renderer, x, y, z, color, 0xFF);
 
-	SDL_RenderPresent(Renderer);
+	SDL_RenderPresent(renderer);
 }
