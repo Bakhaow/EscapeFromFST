05d00cfbc73f2e0c62056901ebc227af0a3ae435
Onin88 <q.beluche@gmail.com>
2022-01-03 12:31:03 +0100

    Suppression Cursor

--- src/main.c
+++ src/main.c
@@ -23,39 +23,6 @@ float calcOffset(float off) {
     return off - ((int)temp - (temp < 0.0f ? 1 : 0)) * 360.0f;
 }
 
-static SDL_Cursor *init_system_cursor(const char *image[])
-{
-  int i, row, col;
-  Uint8 data[4*32];
-  Uint8 mask[4*32];
-  int hot_x, hot_y;
-
-  i = -1;
-  for (row=0; row<32; ++row) {
-    for (col=0; col<32; ++col) {
-      if (col % 8) {
-        data[i] <<= 1;
-        mask[i] <<= 1;
-      } else {
-        ++i;
-        data[i] = mask[i] = 0;
-      }
-      switch (image[4+row][col]) {
-        case 'X':
-          data[i] |= 0x01;
-          mask[i] |= 0x01;
-          break;
-        case '.':
-          mask[i] |= 0x01;
-          break;
-        case ' ':
-          break;
-      }
-    }
-  }
-  sscanf(image[4+row], "%d,%d", &hot_x, &hot_y);
-  return SDL_CreateCursor(data, mask, 32, 32, hot_x, hot_y);
-}
 
 // event handler
 int handle_events(SDL_Event *e, Player *p, SDL_Window *win, Map* m) {
