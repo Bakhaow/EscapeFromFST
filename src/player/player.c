dab8c943193a44d760504494e1da23b8c36196e6
Onin88 <q.beluche@gmail.com>
2022-01-03 12:57:32 +0100

    Supp createPlayer

--- src/player/player.c
+++ src/player/player.c
@@ -1,17 +1,5 @@
 #include "player.h"
 
-Player* createPlayer(int x, int y, int z, int xOff, int yOff) {
-	Player *p = calloc(5, sizeof(Player));
-
-	p->xCoord = x;
-	p->yCoord = y;
-	p->zCoord = z;
-	p->xOffset = xOff;
-	p->yOffset = yOff;
-
-	return p;
-}
-
 void init(Player *p) {
     p->x = 3;
     p->y = 3;
